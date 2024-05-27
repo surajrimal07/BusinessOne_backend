064181f0051c09b72e85a011f60310688a4a235d
Suraj Rimal <davidparkedme@gmail.com>
2024-05-27 08:12:40 +0545

    user added, mogodb switched to atlas

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const path = require("path");
-const dotenv = require("dotenv");
 const dbConnect = require("./database/db");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
-
-dotenv.config();
 
 dbConnect();
 
@@ -33,6 +31,11 @@ app.get("/", (req, res) => {
 app.use("/api/admin", require("./routes/adminRoute"));
 app.use("/api/domain", require("./routes/workDomainRoute"));
 app.use("/api/company", require("./routes/companyRoute"));
+app.use("/api/user", require("./routes/userRoute"));
+
+app.use((req, res, next) => {
+  res.status(404).json({ error: 'Not Found', message: 'The requested resource was not found on this server.' });
+});
 
 
 app.listen(PORT, () => {
