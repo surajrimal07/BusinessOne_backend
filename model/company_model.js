1c1e8656c7f78b972ca21599adccec6b954d7f73
abishek07bimali <abi0shekbimali@gmail.com>
2024-06-17 10:17:05 +0545

    company adding form

--- model/company_model.js
+++ model/company_model.js
@@ -1,93 +1,46 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-// const Schema = mongoose.Schema;
-
-const companySchema = mongoose.Schema({
-    name: String, //company name
-    sections: {
-        introduction: {
-            data: {
-                name: String, //Company Name or known as
-                logo: String, //Company Logo
-                subtitle: String, //company tagline
-                sectors: [{  name: String }],
-                socialLinks: { //social media links
-                    facebook: String,
-                    twitter: String,
-                    linkedin: String
-                },
-                iosAppLink: String, //if the company has an ios app
-                androidLink: String, //if the company has an android app
-                website: String //company website
-            }
-        },
-        basicInformation: {
-            title: String, //"Basic Information",
-            data: {
-                description: String, //Company Description (long)
-                legalName: String, //Legal Name
-                headquarters: String, //Headquarters
-                businessModels: [{ id: String, name: String }], //B2B, B2C, C2C, C2B
-                foundingDate: String, //Founding Date
-                employeeSize: { //Employee Size
-                    id: String,
-                    name: String
-                },
-                // coreTeam: [teamMemberSchema] //Core Team Members
-            }
-        },
-        targetMarket: {
-            title: String, //"Target Market",
-            data: [{
-                geolocations: [{ name: String, id: String }], //India, USA, UK or cities
-                businessSizes: [{ name: String, id: String }], //Small Enterprise, Medium Enterprise, Large Enterprise
-                businessSectors: [{ name: String, id: String }], //Healthcare, Education, Retail
-                ageRanges: [], // 18-24, 25-34, 35-44, 45-54, 55-64, 65+
-                incomeClasses: [] // Lower, Middle, Upper //not applicable for B2B
-            }]
-        },
-        funding: {
-            fundedBy: String, //Venture Capital, Angel Investors, Private Equity, or self-funded
-            investors: [] //List of investors if any
-        },
-        locations: {
-            data: [{
-                cityId: String, //City ID
-                city: String, //City Name
-                state: String, //State Name
-                country: String, //Country Name
-                addressLine1: String,
-                addressLine2: String,
-                googleMapLink: String,
-                headquarters: Boolean //true if this is the headquarter of the company
-            }]
-        },
-        revenueStreams: {
-            title: String, // "title": "Revenue Streams",
-            data: [{
-                id: String, //this is referenced to the revenueStreams array of revenue model
-                name: String
-            }]
-        },
-        products: {
-            title: String, // "title"
-            data: [{
-                id: String,
-                name: String, //Product Name
-                productType: String, //Product or Service
-                description: String, //Product Description
-                website: String, //product website
-                url: String, //product image url
-                sectorName: String, //eg "Construction & Real estate", "Healthcare", "Education"
-                status: String, //Development, Beta, Live, active or inactive
-            }]
-        }
-    },
-    isUserAdmin: Boolean, //true if the user is admin of the company
-    isCompanyClaimed: Boolean, //true if the company is claimed by the owner or aru le haleko?
-    claimedBy: String //userId of the owner if claimed
+const ProductSchema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  image: { type: String },
 });
 
-const company = mongoose.model('company', companySchema);
+const TimelineSchema = new Schema({
+  name: { type: String, required: true },
+  date: { type: Date, required: true },
+  description: { type: String, required: true },
+  image: { type: String },
+});
 
-module.exports = company;
+const FundingSchema = new Schema({
+  name: { type: String, required: true },
+  date: { type: Date, required: true },
+  amount: { type: Number, required: true },
+  description: { type: String },
+});
+
+const CompanySchema = new Schema({
+  name: { type: String, required: true },
+  address: { type: String, required: true },
+  email: { type: String, required: true },
+  phone: { type: String, required: true },
+  category: { type: String, required: true },
+  companyDescription: { type: String, required: true },
+  extendedDate: { type: Date },
+  companyImage: { type: String },
+  contentData: { type: String },
+  website: { type: String },
+  registration: { type: Date },
+  facebook: { type: String },
+  products: [ProductSchema],
+  timelines: [TimelineSchema],
+  fundings: [FundingSchema],
+  basicDescription: { type: String },
+  marketDescription: { type: String },
+  businesstype: { type: String },
+  revenueStream: { type: String },
+});
+
+module.exports = mongoose.model("Company", CompanySchema);
--- routes/companyRoute.js
+++ routes/companyRoute.js
@@ -1,8 +1,16 @@
 const router = require("express").Router();
 const CompanyController = require("../controllers/companyController");
+const uploadImage = require("../helpers/uploadHelper.js");
 
-router.post("/create_company", CompanyController.createCompany);
+router.post(
+  "/create_company",
+  uploadImage.fields([
+    { name: "companyImage", maxCount: 1 },
+    { name: "productImages", maxCount: 10 },
+    { name: "timelineImages", maxCount: 10 },
+  ]),
+  CompanyController.createCompany
+);
 router.get(`/view_details_of_company/:name`, CompanyController.viewCompany);
-
 
 module.exports = router;
